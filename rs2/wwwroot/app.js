9b97d709dd860503a1b5c4236f89382562093c53
Aleksandar Preočanin <preocanin.aleksandar@gmail.com>
2017-04-06 22:24:33 +0200

    Problem solved

--- rs2/wwwroot/app.js
+++ rs2/wwwroot/app.js
@@ -20,36 +20,36 @@ function configRoutes($urlRouterProvider, $stateProvider) {
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: '~/templates/home.html',
+            templateUrl: './templates/home.html',
             controllerAs: 'vm'
         })
         .state('login', {
             url: '/login',
-            templateUrl: '~/templates/login.html',
+            templateUrl: './templates/login.html',
             controller: 'loginRegisterCtrl',
             controllerAs: 'vm'
         })
         .state('register', {
             url: '/register',
-            templateUrl: '~/templates/register.html',
+            templateUrl: './templates/register.html',
             controller: 'loginRegisterCtrl',
             controllerAs: 'vm'
         })
         .state('data', {
             url: '/data',
-            templateUrl: '~/templates/data.html',
+            templateUrl: './templates/data.html',
             controller: 'dataCtrl',
             controllerAs: 'vm'
         })
         .state('clients', {
             url: '/clients',
-            templateUrl: '~/templates/clients.html',
+            templateUrl: './templates/clients.html',
             controller: 'clientsCtrl',
             controllerAs: 'vm'
         })
         .state('clients/:id', {
             url: '/clients/:id',
-            templateUrl: "~/templates/client.html",
+            templateUrl: "./templates/client.html",
             controller: 'clientCtrl',
             controllerAs: 'vm'
         });
